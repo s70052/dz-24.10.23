0234c90d2657273455dbf6330038f71bc5109b2f
эспада8888 <sardorbekkhotamov77@gmail.com>
2023-10-24 16:39:55 +0500

    create repository

---
+++ index.js
@@ -0,0 +1,13 @@
+let nums =[1,2,3,4,5,6,7,8,9]
+
+let evens=nums.filter(num=>num%2===0)
+let odds=nums.filter(num=>num%2!==0)
+
+
+console.log(evens)
+console.log(odds)
+
+let names=['sardor',"tangir","alla","amira","ruslan","daler"]
+
+let evenss=names.filter(names=>names.length%2===0)
+console.log(evenss)
