51fbf542d652d82084eb1858554f64e65602c1f9
=s70052 <=s70052657@gmail.com>
2023-10-25 18:39:02 +0500

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-let nums =[1,2,3,4,5,6,7,8,9]
-
-let evens=nums.filter(num=>num%2===0)
-let odds=nums.filter(num=>num%2!==0)
-
-
-console.log(evens)
-console.log(odds)
-
-let names=['sardor',"tangir","alla","amira","ruslan","daler"]
-
-let evenss=names.filter(names=>names.length%2===0)
-console.log(evenss)
+let arr = ['hello', 1234, '', false, NaN, 123, true, ' ', null]
+let trueElems = [arr.filter(elem => Boolean(elem))]
+console.log(trueElems)
 
 
