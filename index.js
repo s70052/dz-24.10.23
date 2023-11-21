9c81e5ecd2d17efe1c7a1d7290804b15ef933cba
=s70052 <=s70052657@gmail.com>
2023-11-21 13:36:32 +0500

    removed github

--- index.js
+++ index.js
@@ -1666,16 +1666,7 @@ checkAge()
 //         alert('дебил')
 //     }
 
-// function maxName(name1, name2, name3) {
-//     if (name1.length >= name2.length && name1.length >= name3.length) {
-//         console.log(name1)
-//     } else if (name2.length >= name1.length && name2.length >= name3.length) {
-//         console.log(name2);
-//     } else {
-//         console.log(name3)
-//     }
-// }
-// maxName('Alex', 'George', 'Michael');
+
 
 function maxName(name1, name2, name3) {
 
