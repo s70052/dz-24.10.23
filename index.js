e2c16ab47ac80d2f4fcde99b27ef9d7898895020
= <=>
2024-01-06 13:51:13 +0500

    t

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ let success = document.querySelector('.success')
 let form = document.forms.login
 
 
-let one=document.querySelector('#one')
-let two=document.querySelector('#two')
-let three=document.querySelector('#three')
-let four=document.querySelector('#four')
-let five=document.querySelector('#five')
-let six=document.querySelector('#six')
-let seven=document.querySelector('#seven')
-let eight=document.querySelector('#eight')
-let nine=document.querySelector('#nine')
-let eleven=document.querySelector('#eleven')
-let twelve=document.querySelector('#twelve')
+let one = document.querySelector('#one')
+let two = document.querySelector('#two')
+let three = document.querySelector('#three')
+let four = document.querySelector('#four')
+let five = document.querySelector('#five')
+let six = document.querySelector('#six')
+let seven = document.querySelector('#seven')
+let eight = document.querySelector('#eight')
+let nine = document.querySelector('#nine')
+let ten = document.querySelector('#ten')
+let eleven = document.querySelector('#eleven')
+let twelve = document.querySelector('#twelve')
+
 btn.onclick = (event) => {
     event.preventDefault()
     let value = 0
@@ -42,141 +44,159 @@ btn.onclick = (event) => {
             niz[idx].innerHTML = 'Please enter your email adress'
             niz[idx].style.color = 'red'
             svg[idx].style.display = 'block'
+
         } else {
             p[idx].style.color = '#4200FF'
             niz[idx].innerHTML = 'Need to fill'
             niz[idx].style.color = 'gray'
             svg[idx].style.display = 'none'
+
         }
-        
+
     }
     )
-    if (!value > 0) {
+    if (/^[a-z ,.'-]+$/i.test(one.value)) {
+        one.style.border = '3px solid blue'
+        iii.style.display = 'none'
+        ppp.style.color = 'blue'
+    } else {
+        one.style.border = '3px solid red'
+        iii.style.display = 'block'
+        ppp.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(two.value)) {
+        two.style.border = '3px solid blue'
+        iiii.style.display = 'none'
+        pppp.style.color = 'blue'
+    } else {
+        two.style.border = '3px solid red'
+        iiii.style.display = 'block'
+        pppp.style.color = 'red'
+    }
+
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(three.value)) {
+        three.style.border = '3px solid blue'
+        iiiii.style.display = 'none'
+        ppppp.style.color = 'blue'
+    } else {
+        three.style.border = '3px solid red'
+        iiiii.style.display = 'block'
+        ppppp.style.color = 'red'
+    }
+
+    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(four.value)) {
+        four.style.border = '3px solid blue'
+        o.style.color = 'grey'
+    } else {
+        four.style.border = '3px solid red'
+        o.style.color = 'red'
+    }
+
+    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(five.value)) {
+        five.style.border = '3px solid blue'
+        oo.style.color = 'grey'
+    } else {
+        five.style.border = '3px solid red'
+        oo.style.color = 'red'
+    }
+
+    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(six.value)) {
+        six.style.border = '3px solid blue'
+        ooo.style.color = 'grey'
+    } else {
+        six.style.border = '3px solid red'
+        ooo.style.color = 'red'
+    }
+
+    if (/^\S[0-9]{0,3}$/.test(seven.value)) {
+        seven.style.border = '3px solid blue'
+        ic.style.display = 'none'
+        oooo.style.color = 'blue'
+    } else {
+        seven.style.border = '3px solid red'
+        ic.style.display = 'block'
+        oooo.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(eight.value)) {
+        eight.style.border = '3px solid blue'
+        ic2.style.display = 'none'
+        ooooo.style.color = 'blue'
+    } else {
+        eight.style.border = '3px solid red'
+        ic2.style.display = 'block'
+        ooooo.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(nine.value)) {
+        nine.style.border = '3px solid blue'
+        ic3.style.display = 'none'
+        oooooo.style.color = 'blue'
+    } else {
+        nine.style.border = '3px solid red'
+        ic3.style.display = 'block'
+        oooooo.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(ten.value)) {
+        ten.style.border = '3px solid blue'
+        ic4.style.display = 'none'
+        l.style.color = 'blue'
+    } else {
+        ten.style.border = '3px solid red'
+        ic4.style.display = 'block'
+       l.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(eleven.value)) {
+        eleven.style.border = '3px solid blue'
+        ic5.style.display = 'none'
+        ll.style.color = 'blue'
+    } else {
+        eleven.style.border = '3px solid red'
+        ic5.style.display = 'block'
+        ll.style.color = 'red'
+    }
+
+    if (/^[a-z ,.'-]+$/i.test(twelve.value)) {
+        twelve.style.border = '3px solid blue'
+        lll.style.color = 'grey'
+    } else {
+        twelve.style.border = '3px solid red'
+        lll.style.color = 'red'
+    }
+
+
+    if (value === 0) {
         submit()
-    } else{
+    } else {
         alert('error')
     }
 
 }
-let pp=document.querySelector('#p')
-one.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(one.value)) {
-        one.style.border = '3px solid blue'
+let lll = document.querySelector('#lll')
+let ll = document.querySelector('#ll')
+let l = document.querySelector('#l')
+let oooooo = document.querySelector('#oooooo')
+let ooooo = document.querySelector('#ooooo')
+let oooo = document.querySelector('#oooo')
+let ooo = document.querySelector('#ooo')
+let oo = document.querySelector('#oo')
+let o = document.querySelector('#o')
+let ic = document.querySelector('#ic')
+let ic2 = document.querySelector('#ic2')
+let ic3 = document.querySelector('#ic3')
+let ic4 = document.querySelector('#ic4')
+let ic5 = document.querySelector('#ic5')
+let iii = document.querySelector('#iii')
+let iiii = document.querySelector('#iiii')
+let iiiii = document.querySelector('#iiiii')
+let ppp = document.querySelector('#p')
+let pppp = document.querySelector('#pp')
+let ppppp = document.querySelector('#ppp')
 
-    } else {
-        one.style.border = '3px solid red'
-        pp.style.color='1px solid red'
-        inp.style.border='3px solid red'
-    }
-}
-two.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(two.value)) {
-        two.style.border = '3px solid blue'
 
-    } else {
-        two.style.border = '3px solid red'
-
-    }
-}
-three.onkeyup = () => {
-    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(three.value)) {
-        three.style.border = '3px solid blue'
-
-    } else {
-        three.style.border = '3px solid red'
-        inp.style.border='3px solid red'
-    }
-}
-four.onkeyup = () => {
-    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(four.value)) {
-        four.style.border = '3px solid blue'
-
-    } else {
-        four.style.border = '3px solid red'
-
-    }
-}
-five.onkeyup = () => {
-    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(five.value)) {
-        five.style.border = '3px solid blue'
-
-    } else {
-        five.style.border = '3px solid red'
-
-    }
-}
-six.onkeyup = () => {
-    if (/\+\((?:998\))([0-9]{2})[-]([0-9]{3})[-](\d{2})[-](\d{2})/gm.test(six.value)) {
-        six.style.border = '3px solid blue'
-
-    } else {
-        six.style.border = '3px solid red'
-
-    }
-}
-seven.onkeyup = () => {
-    if (/^\S[0-9]{0,3}$/.test(seven.value)) {
-        seven.style.border = '3px solid blue'
-
-    } else {
-        seven.style.border = '3px solid red'
-
-    }
-}
-eight.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(eight.value)) {
-        eight.style.border = '3px solid blue'
-
-    } else {
-        eight.style.border = '3px solid red'
-       
-    }
-}
-nine.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(nine.value)) {
-        nine.style.border = '3px solid blue'
-
-    } else {
-        nine.style.border = '3px solid red'
-       
-    }
-}
-ten.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(ten.value)) {
-        ten.style.border = '3px solid blue'
-
-    } else {
-        ten.style.border = '3px solid red'
-       
-    }
-}
-eleven.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(eleven.value)) {
-        eleven.style.border = '3px solid blue'
-
-    } else {
-        eleven.style.border = '3px solid red'
-       
-    }
-}
-eleven.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(eleven.value)) {
-        eleven.style.border = '3px solid blue'
-
-    } else {
-        eleven.style.border = '3px solid red'
-       
-    }
-}
-twelve.onkeyup = () => {
-    if (/^[a-z ,.'-]+$/i.test(twelve.value)) {
-        twelve.style.border = '3px solid blue'
-
-    } else {
-        twelve.style.border = '3px solid red'
-       
-    }
-}
 function submit() {
     let user = {}
 
