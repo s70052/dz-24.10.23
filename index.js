359824da3c90cabe1b18eb1aab7de13646b3215a
espada8888 <sardorbekkhotamov77@gmail.com>
2024-02-08 15:58:28 +0500

    r

--- index.js
+++ index.js
@@ -1,324 +1,49 @@
-let arr = [{
-    "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 ",
-    "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday  three-button placket",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": {
-        "rate": 3.9,
-        "count": 120
-    }
-}, {
-    "id": 2,
-    "title": "Mens Casual Premium Slim  ",
-    "price": 22.3,
-    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric The Henley style round neckline includes a three-button placket.",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    "rating": {
-        "rate": 4.1,
-        "count": 259
-    }
-}, {
-    "id": 3,
-    "title": "Mens Cotton Jacket",
-    "price": 55.99,
-    "description": "great outerwear jackets for Spring/Autumn/Winter,  Good gift  for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving ",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-    "rating": {
-        "rate": 4.7,
-        "count": 500
-    }
-}, {
-    "id": 4,
-    "title": "Mens Casual Slim Fit",
-    "price": 15.99,
-    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size .",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-    "rating": {
-        "rate": 2.1,
-        "count": 430
-    }
-}, {
-    "id": 5,
-    "title": "John Hardy Women's Lege ",
-    "price": 695,
-    "description": "From our Legends Collection, the Naga was inspired by the s pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-    "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-    "rating": {
-        "rate": 4.6,
-        "count": 400
-    }
-}, {
-    "id": 6,
-    "title": "Solid Gold Petite Micropave ",
-    "price": 168,
-    "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed s. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-    "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-    "rating": {
-        "rate": 3.9,
-        "count": 70
-    }
-}, {
-    "id": 7,
-    "title": "White Gold Plated ",
-    "price": 9.99,
-    "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-    "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
-    "rating": {
-        "rate": 3,
-        "count": 400
-    }
-}, {
-    "id": 8,
-    "title": "Pierced Owl Rose",
-    "price": 10.99,
-    "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel transfersfor   vary depending on user’s hardware configuration transfersfor ",
-    "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
-    "rating": {
-        "rate": 1.9,
-        "count": 100
-    }
-}, {
-    "id": 9,
-    "title": "WD 2TB Elements ",
-    "price": 64,
-    "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfersfor other operating systems; Compatibility may vary depending on user’s hardware configuration ",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-    "rating": {
-        "rate": 3.3,
-        "count": 203
-    }
-}, {
-    "id": 10,
-    "title": "SanDisk SSD PLUS 1TB ",
-    "price": 109,
-    "description": "Easy upgrade for faster boot up, shutdown, application lo for typical PC workloads The perfect balance of performance and reliabilitydevice,)",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
-    "rating": {
-        "rate": 2.9,
-        "count": 470
-    }
-}, {
-    "id": 11,
-    "title": "Silicon Power 256GB ",
-    "price": 109,
-    "description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds  Ultrabooks and Ultra-slim notebooksnd enhanced reliability.",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
-    "rating": {
-        "rate": 4.8,
-        "count": 319
-    }
-}, {
-    "id": 12,
-    "title": "WD 4TB Gaming Drive",
-    "price": 114,
-    "description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
-    "rating": {
-        "rate": 4.8,
-        "count": 400
-    }
-}, {
-    "id": 13,
-    "title": "Acer SB220Q bi 21.5 ",
-    "price": 599,
-    "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display  Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree.",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
-    "rating": {
-        "rate": 2.9,
-        "count": 250
-    }
-}, {
-    "id": 14,
-    "title": "Samsung 49-Inch CHG90  ",
-    "price": 999.99,
-    "description": "49 INCH SUPER  HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response ",
-    "category": "electronics",
-    "image": "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
-    "rating": {
-        "rate": 2.2,
-        "count": 140
-    }
-}, {
-    "id": 15,
-    "title": "BIYLACLESEN Women's ",
-    "price": 56.99,
-    "description": "Note:The Jackets is US standard  Functional Liner: n separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-    "rating": {
-        "rate": 2.6,
-        "count": 235
-    }
-}, {
-    "id": 16,
-    "title": "Lock and Love Women's ",
-    "price": 29.95,
-    "description": "100% POLYURETHANE(shell) 100% POLYESTER(lining)  style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY ",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-    "rating": {
-        "rate": 2.9,
-        "count": 340
-    }
-}, {
-    "id": 17,
-    "title": "Rain Jacket Women ",
-    "price": 39.99,
-    "description": "Lightweight perfet for triphold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo  Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
-    "rating": {
-        "rate": 3.8,
-        "count": 679
-    }
-}, {
-    "id": 18,
-    "title": "MBJ Women's Solid ",
-    "price": 9.85,
-    "description": "95% RAYON 5% SPANDEX, Made in USA or Imported,  with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
-    "rating": {
-        "rate": 4.7,
-        "count": 130
-    }
-}, {
-    "id": 19,
-    "title": "Opna Women's Short ",
-    "price": 7.95,
-    "description": "100% Polyester,  Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
-    "rating": {
-        "rate": 4.5,
-        "count": 146
-    }
-}, {
-    "id": 20,
-    "title": "DANVOUY Womens T Shirt ",
-    "price": 12.99,
-    "description": "95%Cotton,5%Spandex, Features: Casual, Short : Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter  three-button placket.",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
-    "rating": {
-        "rate": 3.6,
-        "count": 145
-    }
-}]
+let submit = document.querySelector('.submit')
+let big = document.querySelector('.big')
 
+submit.onclick = () => {
+    let text = document.querySelector('.text')
+    let value = text.value
 
-for (let i = 0; i < 20; i++) {
+    if (value !== "") {
+        //a
+        let mini_box = document.createElement('div')
+        let pokup = document.createElement('div')
+        let p = document.createElement('p')
+        let img = document.createElement('img')
+        let span = document.createElement('span')
+        //b
 
-    //a
+        text.value = ""
+        span.innerHTML = '18:44'
+    
+        mini_box.classList.add('mini_box')
+        span.classList.add('minutes')
+        pokup.classList.add('pokup')
+        img.src = './xbutton_87873.png'
+        img.classList.add('del')
+        p.innerHTML = value + ':)'
+        p.style.fontFamily = 'Italiannо'
+        //c
+        big.append(mini_box)
+        mini_box.append(pokup,span)
+        pokup.append(p, img)
+        img.onclick = () => {
+            mini_box.remove()
+        }
+      
+    }
+ 
+}
 
-    let wrap = document.querySelector('.wrap')
-    let box = document.createElement('div')
-    let ml_box = document.createElement('div')
-    let img = document.createElement('img')
-
-    let black_box = document.createElement('div')
-    let black_box_h1 = document.createElement('h1')
-    let black_box_p = document.createElement('p')
-
-    let flex = document.createElement('div')
-    let fle_box = document.createElement('div')
-    let img2 = document.createElement('img')
-    let fle_box_h2 = document.createElement('h2')
-
-    let fle_box2 = document.createElement('div')
-    let img3 = document.createElement('img')
-    let fle_box_h22 = document.createElement('h2')
-
-    let fle_box3 = document.createElement('div')
-    let img4 = document.createElement('img')
-    let fle_box_h23 = document.createElement('h2')
-
-    let btns = document.createElement('div')
-
-    let btn = document.createElement('button')
-    //b
-
-    box.classList.add('box')
-    ml_box.classList.add('ml_box')
-    img.classList.add('img')
-    black_box.classList.add('black_box')
-    black_box_h1.classList.add('h1')
-    black_box_p.classList.add('p')
-    flex.classList.add('flex')
-    fle_box.classList.add('fle_box')
-    img2.classList.add('img')
-    fle_box_h2.classList.add('h2')
-
-    fle_box2.classList.add('fle_box')
-    img3.classList.add('img')
-    fle_box_h22.classList.add('h2')
-
-    fle_box3.classList.add('fle_box')
-    img4.classList.add('img')
-    fle_box_h23.classList.add('h2')
-
-    btns.classList.add('btns')
-    btn.classList.add('btn')
-
-    fle_box_h2.innerHTML = arr[i].price
-    fle_box_h22.innerHTML = arr[i].rating.rate
-    fle_box_h23.innerHTML = arr[i].rating.count
-    black_box_h1.innerHTML = arr[i].title
-    black_box_p.innerHTML = arr[i].description
-    btn.innerHTML = 'В избранное'
-    img.src = arr[i].image
-
-
-    img2.src = './Group 7 (2).png'
-    img3.src = './Group 10.svg'
-    img4.src = './Group 9.png'
-
-
-    wrap.append(box)
-    box.append(ml_box, black_box)
-    ml_box.append(img)
-    black_box.append(black_box_h1, black_box_p, flex, btns)
-    flex.append(fle_box, fle_box2, fle_box3)
-    fle_box.append(img2, fle_box_h2)
-    fle_box2.append(img3, fle_box_h22)
-    fle_box3.append(img4, fle_box_h23)
-    btns.append(btn)
-
+let del = document.querySelector('.del')
+let mini_box = document.querySelector('.mini_box')
+del.onclick = () => {
+    mini_box.remove()
 }
 
 
-let bt = document.querySelectorAll('.btn')
-bt.forEach(btn => {
-    bt[1].innerHTML = 'Добавленно'
-    bt[1].classList.add('zzz')
-    btn.onclick = () => {
-        if (btn.classList.contains('zzz')) {
-            btn.classList.remove('zzz')
-            btn.innerHTML = 'В избранное'
-        } else {
-            btn.classList.add('zzz')
-            btn.innerHTML = 'Добавленно'
-        }
-    }
-})
 
 
 
-
-
-// Покажите товары по следующему массиву
 
