6151d82b260801168f0c5bd7c07f84bd8c48ef76
espada8888 <sardorbekkhotamov77@gmail.com>
2024-02-10 14:07:17 +0500

    r

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let big = document.querySelector('.big')
 submit.onclick = () => {
     let text = document.querySelector('.text')
     let value = text.value
-
+    text.value = ""
     if (value !== "") {
         //a
         let mini_box = document.createElement('div')
@@ -14,20 +14,27 @@ submit.onclick = () => {
         let span = document.createElement('span')
         //b
 
-        text.value = ""
-        span.innerHTML = '18:44'
+        
+      let time=new Date()
+      let cur=time.getHours()+':'+time.getMinutes()+':'+time.getSeconds()
     
         mini_box.classList.add('mini_box')
         span.classList.add('minutes')
+        span.classList.add('p')
         pokup.classList.add('pokup')
+       span.style.color='white'
         img.src = './xbutton_87873.png'
         img.classList.add('del')
         p.innerHTML = value + ':)'
-        p.style.fontFamily = 'Italiannо'
+        span.innerHTML=cur
+     
         //c
         big.append(mini_box)
         mini_box.append(pokup,span)
         pokup.append(p, img)
+
+     
+
         img.onclick = () => {
             mini_box.remove()
         }
@@ -35,9 +42,15 @@ submit.onclick = () => {
     }
  
 }
+let min=document.querySelector('.minutes')
 
+let tim=new Date()
+let cu=tim.getHours()+':'+tim.getMinutes()+':'+tim.getSeconds()
+min.innerHTML=cu
+min.style.color = 'white'
 let del = document.querySelector('.del')
 let mini_box = document.querySelector('.mini_box')
+let v=
 del.onclick = () => {
     mini_box.remove()
 }
