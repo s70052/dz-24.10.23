3f75399ff8be8a8f4df8060dcb564e7500b28b88
espada8888 <sardorbekkhotamov77@gmail.com>
2024-03-01 15:47:55 +0500

    rr

---
+++ script.js
@@ -0,0 +1,225 @@
+// for(let i=0;i<4;i++){
+
+// let box=document.createElement('div')
+// let tem=document.createElement('div')
+// let bel=document.createElement('div')
+
+// box.classList.add("box")
+// tem.classList.add("tem")
+// bel.classList.add("bel")
+
+// document.body.append(box)
+// box.append(tem,bel)
+// }
+
+let wrap=document.querySelector('.wrap')
+let box = document.createElement('div')
+let left = document.createElement('div')
+let p = document.createElement('p')
+let pp = document.createElement('p')
+let ppp = document.createElement('p')
+
+let right = document.createElement('div')
+let u = document.createElement('p')
+let uu = document.createElement('p')
+let uuu = document.createElement('p')
+// let call=document.createElement('img')
+let uuuu = document.createElement('p')
+
+let box_2 = document.createElement('div')
+let logo = document.createElement('div')
+
+let bix = document.createElement('div')
+let log = document.createElement('img')
+
+let lupa = document.createElement('div')
+let log2 = document.createElement('img')
+let log_p = document.createElement('p')
+
+let inn = document.createElement('div')
+let inp = document.createElement('input')
+let red = document.createElement('div')
+let find = document.createElement('img')
+
+let bix_2 = document.createElement('div')
+let ix_img = document.createElement('div')
+let img = document.createElement('img')
+let img2 = document.createElement('img')
+let img3 = document.createElement('img')
+let img4 = document.createElement('img')
+let img5 = document.createElement('img')
+
+let ix_p = document.createElement('div')
+let wp = document.createElement('p')
+let w = document.createElement('p')
+let ww = document.createElement('p')
+let www = document.createElement('p')
+let wwww = document.createElement('p')
+
+let i = document.createElement('div')
+
+let ii = document.createElement('p')
+let iii = document.createElement('p')
+let iiii = document.createElement('p')
+let iiiii = document.createElement('p')
+let iiiiii = document.createElement('p')
+let o = document.createElement('p')
+let oo = document.createElement('p')
+let ooo = document.createElement('p')
+
+box.classList.add('box')
+left.classList.add('left')
+p.innerHTML = 'Москва'
+pp.innerHTML = 'Магазины'
+ppp.innerHTML = 'Доставка'
+
+right.classList.add('right')
+u.innerHTML = 'Блог «М.Клик»'
+uu.innerHTML = 'M.Club'
+uuu.innerHTML = 'Для бизнеса'
+// call.src=''
+uuuu.innerHTML = '8-800-600-7775'
+
+box_2.classList.add('box_2')
+logo.classList.add('logo')
+bix.classList.add('bix')
+
+
+
+log.classList.add('log')
+log.src = 'https://cms.mvideo.ru/magnoliaPublic/dam/jcr:f57062ef-960d-4458-b10e-314bf7573980'
+lupa.classList.add('lupa')
+log2.src='./img/Аннотация 2024-02-29 185906.png'
+log_p.innerHTML='Каталог'
+log2.classList.add('lu')
+inn.classList.add('in')
+inp.classList.add('inp')
+red.classList.add('red')
+find.src='./img/1814075_find_magnifier_magnifying glass_search_icon.png'
+find.classList.add('find')
+
+
+bix_2.classList.add('bix_2')
+ix_img.classList.add('ix_img')
+img.src='./img/11419900_oclock_icon.png'
+img2.src='./img/5340287_man_people_person_user_users_icon.png'
+img3.src='./img/Аннотация 2024-01-11 151323.png'
+img4.src='./img/211754_heart_icon.png'
+img5.src='./img/2849824_store_shopping_market_buy_shop_icon.png'
+
+
+ix_p.classList.add('ix_p')
+wp.classList.add('p')
+wp.innerHTML='Статус заказа'
+w.innerHTML='Войти'
+ww.innerHTML='Сравнение'
+www.innerHTML='Избранное'
+wwww.innerHTML='Корзина'
+
+i.classList.add('pp')
+ii.innerHTML='ВСЕ АКЦИИ'
+iii.innerHTML='СКИДКИ НА ПОДАРКИ'
+iiii.innerHTML='SAMSUNG S24 + КЭШБЭК 10 000₽'
+iiiii.innerHTML='НОУТБУКИ'
+iiiiii.innerHTML='УСПЕЙ КУПИТЬ'
+o.innerHTML='ХОЛОДИЛЬНИКИ'
+oo.innerHTML='ТЕЛЕВИЗОРЫ'
+ooo.innerHTML='PREMIUM'
+
+wrap.append(box,box_2,i)
+box.append(left,right)
+left.append(p,pp,ppp)
+right.append(u,uu,uuu,uuuu)
+box_2.append(logo)
+logo.append(bix,bix_2)
+bix.append(log,lupa,inn)
+lupa.append(log2,log_p)
+inn.append(inp,red)
+red.append(find
+    )
+bix_2.append(ix_img,ix_p)
+ix_img.append(img,img2,img3,img4,img5)
+ix_p.append(wp,w,ww,www,wwww)
+i.append(ii,iii,iiii,iiiii,iiiiii,o,oo,ooo)
+
+
+
+ 
+let boc=document.querySelector('.boc')
+
+let br=document.createElement('div')
+let tem=document.createElement('button')
+let arr=document.createElement('button')
+let arro=document.createElement('img')
+let aad=document.createElement('img')
+let fen=document.createElement('div')
+let fwwn=document.createElement('span')
+let ccc=document.createElement('div')
+let star=document.createElement('img')
+let rr=document.createElement('span')
+let ot=document.createElement('span')
+let cs=document.createElement('div')
+let rub=document.createElement('span')
+let re=document.createElement('span')
+let blck=document.createElement('div')
+
+let bd=document.createElement('div')
+let cash=document.createElement('span')
+let num=document.createElement('span')
+let ima=document.createElement('div')
+let vw=document.createElement('div')
+let vw_img=document.createElement('img')
+let sa=document.createElement('img')
+let saa=document.createElement('img')
+
+br.classList.add('br')
+tem.classList.add('tem')
+arr.classList.add('arr')
+arro.classList.add('arro')
+aad.classList.add('add')
+fen.classList.add('fen')
+fwwn.classList.add('fwwn')
+ccc.classList.add('ccc')
+rr.classList.add('rr')
+ot.classList.add('ot')
+cs.classList.add('cs')
+rub.classList.add('rub')
+re.classList.add('re')
+blck.classList.add('blck')
+bd.classList.add('bd')
+cash.classList.add('cash')
+num.classList.add('num')
+ima.classList.add('ima')
+vw.classList.add('vw')
+sa.classList.add('sa')
+saa.classList.add('sa')
+ 
+
+
+tem.innerHTML='Топ-скидки до 20%'
+arro.src='./img/211688_forward_arrow_icon.png'
+aad.src='https://img.mvideo.ru/Pdb/small_pic/200/20086989b.jpg'
+fwwn.innerHTML='Fen Carrera №222'
+star.src='./img/star.png'
+rr.innerHTML='5,0'
+ot.innerHTML='6 otzivov'
+rub.innerHTML='3 999'
+re.innerHTML='7 999'
+blck.innerHTML=' -50%'
+cash.innerHTML='Кешбек'
+num.innerHTML='+200'
+vw_img.src='./img/605508_bag_buy_cart_item_sale_icon.png'
+sa.src='./img/211754_heart_icon.png'
+saa.src='./img/Аннотация 2024-01-11 151323.png'
+
+boc.append(br,aad,fen,ccc,cs,bd,ima)
+br.append(tem,arr)
+fen.append(fwwn)
+ccc.append(star,rr,ot)
+cs.append(rub, re, blck)
+bd.append(cash,num)
+ima.append(vw,sa,saa)
+arr.append(arro)
+vw.append(vw_img)
+
+
