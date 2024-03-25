245a194d9ed5e8fa23511b44c29ad1b9d753f2f4
espada8888 <sardorbekkhotamov77@gmail.com>
2024-03-25 16:46:20 +0500

    ee

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
 
+const form = document.forms.namedItem('login')
+
+const inputs = form.querySelectorAll('input')
+const p = document.querySelector('.di_p')
+let b = document.querySelector('#name')
+let age = document.querySelector('.age')
+const oop = document.querySelector('.op')
+const up = document.querySelector('.up')
+const namee = document.querySelector('.name')
+const patterns = {
+    name: /^[a-zA-Z]{3,10}$/i,
+    age: /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/,
+
+}
 
 
+inputs.forEach((inp) => {
+    inp.onkeyup = () => {
+       
+        let b='От 3 до 10 букв'
+        if (!patterns[inp.name].test(inp.value)) {
+            inp.style.border = '2px solid red'
+            up.innerHTML = 'Введите от 2 до 10 букв'
+          
+        } else {
+            inp.style.border = '2px solid blue'
+            up.innerHTML = ''
+          
+        }
+    }
+})
 
-const form = document.forms.login;
-const nam = document.querySelector('.name')
-const age = document.querySelector('.age')
-
-
-
-
+const modal_content = document.querySelector('dialog')
 form.onsubmit = (e) => {
     e.preventDefault()
-
+    let error = false
     let fm = new FormData(form)
     let user = {}
 
     fm.forEach((val, key) => {
         user[key] = val
+        if (val === 'not') error = true
     })
-    if (nam.value && age.value !== '') {
-        modal.style.display = 'block'
-        console.log(user);
-    } else {
-        alert('Заполни все поля')
+    inputs.forEach(inp => {
+        if (inp.value.length === 0) {
+            error = true
+        }
+    })
+    if (error) {
+        alert('dddd')
+        return
     }
+    b.innerHTML = user.name
+    p.innerHTML = `Через 10 лет ты станешь ${user.language} разработчиком`
+    modal_content.showModal()
+
 }
 
-const modal = document.querySelector('#myModal')
-const modal_content = document.querySelector('.modal-content')
-const select = document.querySelector('select')
-const p = document.querySelector('p')
+
+
 const closee = document.querySelector('.close')
 
-select.onclick = (event) => {
-    const option = event.target.value
-
-    modal_content.classList.add('f')
-    p.innerHTML = 'Привет'+' ' + nam.value+' ' + 'Через 10 лет вы станете' + ' ' + option + ' ' + 'разработчиком'
-}
-// const options = document.querySelectorAll('option');
-
-// options.forEach(option => {
-//     option.onclick = (event) => {
-//         let selectedValue = event.target.value;
-
-//         modal_content.classList.add('f')
-//         p.innerHTML = 'Через 10 лет вы станете' + ' ' + selectedValue + ' ' + 'разработчиком'
-//     }
-// })
 
 closee.onclick = () => {
-    modal.style.display = 'none'
+    modal_content.close()
+
 }
 
