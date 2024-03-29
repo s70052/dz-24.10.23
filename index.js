14fb2a4bf2403a895fe9f608d102988180de7104
espada8888 <sardorbekkhotamov77@gmail.com>
2024-03-29 15:53:50 +0500

    ee

--- index.js
+++ index.js
@@ -1,96 +1,70 @@
-const form = document.forms.namedItem('login');
-const inputs = form.querySelectorAll('input');
-let errors = document.querySelector('.errors');
-let errorCount = 0
-let success = document.querySelector('.success')
-let success_count = 0
-let b = document.querySelector('#name')
-let p = document.querySelector('.di_p')
-let stat = false
-let v=document.querySelector('.All')
-const patterns = {
-    name: /^[a-zA-Z]{3,10}$/i,
-    age: /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/,
-}
-success_count = inputs.length
-inputs.forEach((inp) => {
-    inp.oninput = () => {
-        let label = inp.parentNode;
-        let span = document.querySelector('span');
 
-        if (!patterns[inp.name].test(inp.value)) {
-            if (!span.classList.contains('error1')) {
-
-                errorCount++
-                success_count--
-            }
-            inp.style.border='2px solid red'
-            label.classList.add('error1')
-            span.classList.add('error1')
-            span.style.display = 'flex'
-            stat = true
-        } else {
-            if (span.classList.contains('error1')) {
-                errorCount--
-                success_count++
-            }
-            label.classList.remove('error1')
-            span.classList.remove('error1')
-            inp.style.border='2px solid blue'
-            span.style.display = 'none'
-            stat = false
-        }
-        errors.innerHTML = errorCount
-        success.innerHTML = success_count
-       
-    }
-    v.innerHTML=inputs.length
-})
-
-
-
-
-
-const modal_content = document.querySelector('dialog')
+let form = document.forms.namedItem('login')
+let tb = document.querySelector('.tb')
+let inp=document.querySelectorAll('input')
+let el = []
 form.onsubmit = (e) => {
     e.preventDefault()
-    let error = false
-    let fm = new FormData(form)
-    let user = {}
-
-    fm.forEach((val, key) => {
-        user[key] = val
-        if (val === 'not') error = true
+    let age = new FormData(form).get('age')
+    let a = 2024 - age
+    let user = {
+        name: new FormData(form).get('name'),
+        year: a,
+        id:Math.random()
+    }
+    el.push(user)
+    console.log(user)
+    inp.forEach(item=>{
+        item.value=''
     })
-    inputs.forEach(inp => {
-        if (inp.value.length === 0) {
-            error = true
+    reload(el, tb)
+}
+
+function reload(arr, place) {
+    place.innerHTML = ''
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i]
+        let table = document.createElement('table')
+        let tr = document.createElement('tr')
+        let th = document.createElement('th')
+        let th2 = document.createElement('th')
+        let th3 = document.createElement('th')
+        let th4 = document.createElement('th')
+        let imgs = document.createElement('div')
+        let change = document.createElement('img')
+        let trash = document.createElement('img')
+
+        table.classList.add('table')
+        tr.classList.add('tr')
+        th.classList.add('th', 'e')
+        th.innerHTML = i+1
+        th2.classList.add('thh')
+        th2.innerHTML = item.name
+        th3.classList.add('thhh')
+        th3.innerHTML = item.year
+        th4.classList.add('th')
+        imgs.classList.add('imgs')
+        change.classList.add('change')
+        change.src = './8666681_edit_icon.svg'
+        trash.src = './4781812_bin_delete_file_garbage_recycle_icon.png'
+        trash.classList.add('trash')
+
+        place.appendChild(table)
+        table.appendChild(tr)
+        tr.append(th, th2, th3, th4)
+        th4.append(imgs)
+        imgs.append(change, trash)
+
+  
+
+        trash.onclick = () => {
+            table.classList.add('aa')
+
+            setTimeout(() => {
+                table.remove()
+            }, 500)
         }
-    })
-    if (error) {
-        alert('Заполните все поля')
-        return
     }
-
-    if (stat !== true) {
-
-        b.innerHTML = user.name
-        p.innerHTML = `Через 10 лет ты станешь ${user.language} разработчиком`
-        modal_content.showModal()
-
-    }else{
-        alert('Заполните все правильно')
-    }
-
 }
 
-
-
-const closee = document.querySelector('.close')
-
-
-closee.onclick = () => {
-    modal_content.close()
-
-}
-
+reload(el, tb)
